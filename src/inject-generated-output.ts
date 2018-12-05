8715e0e55036c30e71d28d73b1159603aca97360
Meno Abels <meno.abels@adviser.com>
2018-12-05 16:22:11 +0100

    fix: remove dependency to graphql on inject-generated-output

--- src/inject-generated-output.ts
+++ src/inject-generated-output.ts
@@ -1,20 +1,13 @@
-import { GraphQLObjectType } from 'graphql';
-import { GenerateOutputType } from './graphql-output-from-type';
-
 export class InjectGeneratedOutput {
   private injected: string;
-  private readonly type: GraphQLObjectType;
 
-  public constructor(t: GraphQLObjectType) {
-    this.type = t;
-  }
+  public constructor() {}
 
-  public inject(cb?: GenerateOutputType): string {
+  public inject(cb?: () => string): string {
     if (this.injected) {
       return this.injected;
     }
-    this.injected = cb!(this.type);
+    this.injected = cb!();
     return this.injected;
   }
-
 }
